bb7710a427968656ec8e4eb24fcf8586bc0fb3f8
HR Williams <halrubinwilliams@gmail.com>
2021-06-21 23:46:27 -0700

    Allow users to view all flavors on index, allow logged in user to create new flavor

---
+++ PierresSweetAndSavoryTreats/Controllers/FlavorController.cs
@@ -0,0 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using PierresSweetAndSavoryTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace PierresSweetAndSavoryTreats.Controllers
+{
+  [Authorize]
+  public class FlavorsController : Controller{
+    private readonly PierresSweetAndSavoryTreatsContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public FlavorsController(UserManager<ApplicationUser> userManager,PierresSweetAndSavoryTreatsContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    [AllowAnonymous]
+    public ActionResult Index()
+    {
+      var flavors = _db.Flavors.ToList();
+      return View (flavors);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(Flavor flavor, int TreatId)
+    {
+        var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var currentUser = await _userManager.FindByIdAsync(userId);
+        flavor.User = currentUser;
+        _db.Flavors.Add(flavor);
+        _db.SaveChanges();
+        if (TreatId != 0)
+        {
+            _db.FlavorTreat.Add(new FlavorTreat() { TreatId = TreatId, FlavorId = flavor.FlavorId });
+        }
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+  }
+}

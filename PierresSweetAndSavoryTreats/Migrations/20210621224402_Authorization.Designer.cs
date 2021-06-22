fc3021694489ba43bb8e9d7b0038ac41061d31b1
HR Williams <halrubinwilliams@gmail.com>
2021-06-21 17:49:28 -0700

    Make sure remote is up to date with local:

--- PierresSweetAndSavoryTreats/Migrations/20210621224402_Authorization.Designer.cs
+++ PierresSweetAndSavoryTreats/Migrations/20210621224402_Authorization.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PierresSweetAndSavoryTreats.Models;
 
 namespace PierresSweetAndSavoryTreats.Migrations
 {
     [DbContext(typeof(PierresSweetAndSavoryTreatsContext))]
-    partial class PierresSweetAndSavoryTreatsContextModelSnapshot : ModelSnapshot
+    [Migration("20210621224402_Authorization")]
+    partial class Authorization
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
